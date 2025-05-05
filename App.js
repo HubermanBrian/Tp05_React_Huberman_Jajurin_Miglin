f372a982d2445b3c58525b6445384d48a0d0d2fd
BRIAN GABRIEL HUBERMAN <48179712@ort>
2025-05-05 12:08:36 -0300

    no se integra la foto, tp finalizado

--- App.js
+++ App.js
@@ -1,7 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const esperarAlgunosMilisegundos = async () => {
+      // Evitar que el splash screen se oculte automáticamente
+      await SplashScreen.preventAutoHideAsync();
+      try {
+          // Acá hacemos todas las llamadas asincrónicas que necesitamos.
+          // Por ejemplo precarga de Fonts, llamadas a APIs externas, etc...
+          
+          // Simular una demora de 5 segundos.
+          await new Promise(resolve => setTimeout(resolve, 5000));
+      } catch (e) {
+          console.warn(e);
+      } finally {
+          // Cambiar el estado de carga a falso y ocultar el splash screen
+          setIsLoading(false);
+          await SplashScreen.hideAsync();
+      }
+    };
+
+    esperarAlgunosMilisegundos();
+  }, []);
+
+  // Renderizado condicional:
+  // Mostrar una pantalla en blanco mientras se carga
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,6 +41,8 @@ export default function App() {
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
